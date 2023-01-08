08754c5c670342287fd3fdc9f3ea9adbf8318393
samdann <sami.dannoune@gmail.com>
2023-01-08 13:44:49 +0100

    external dependencies for Swagger-ui and first endpoint defined

---
+++ src/main/java/org/blackchain/PortfolioTrackerServiceApplication.java
@@ -0,0 +1,30 @@
+package org.blackchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication(exclude = {RepositoryRestMvcAutoConfiguration.class,
+        SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+public class PortfolioTrackerServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioTrackerServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
---
+++ src/main/java/org/blackchain/controller/AccountController.java
@@ -0,0 +1,38 @@
+package org.blackchain.controller;
+
+import io.api.etherscan.core.impl.EtherScanApi;
+import io.api.etherscan.model.Balance;
+import io.micrometer.core.annotation.Timed;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@Timed
+@RestController
+public class AccountController {
+
+    @ApiOperation(value = "account-balance", tags = "account-api")
+    @RequestMapping(
+            value = "/_account-balance",
+            produces = {"application/json"},
+            method = RequestMethod.GET
+    )
+    public ResponseEntity<String> getAccountBalance(
+            @ApiParam(name = "apiKey", value = "Api Key") @RequestParam(value = "apiKey", required = true) final String apiKey,
+            @ApiParam(name = "address", value = "Wallet Address") @RequestParam(value = "address", required = true) final String address) {
+
+        EtherScanApi api = new EtherScanApi(apiKey);
+        Balance balance = api.account().balance(address);
+
+        return ResponseEntity.ok().body(balance.getEther().toString());
+
+    }
+}
