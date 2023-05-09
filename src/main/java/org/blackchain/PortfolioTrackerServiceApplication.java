aa7cba95ce15a730642a610933e9b95cb628d73a
samdann <sami.dannoune@gmail.com>
2023-05-09 23:32:15 +0100

    docker file for build

--- src/main/java/org/blackchain/PortfolioTrackerServiceApplication.java
+++ src/main/java/org/blackchain/PortfolioTrackerServiceApplication.java
@@ -2,13 +2,20 @@ package org.blackchain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
-public class PortfolioTrackerServiceApplication {
+public class PortfolioTrackerServiceApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioTrackerServiceApplication.class, args);
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*");
     }
 
 
--- src/main/java/org/blackchain/controller/AccountController.java
+++ src/main/java/org/blackchain/controller/AccountController.java
@@ -2,14 +2,24 @@ package org.blackchain.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import java.io.IOException;
+import java.math.BigDecimal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Convert.Unit;
 
 @RestController
 public class AccountController {
+
+    private static final String RPC_PROVIDER = "https://rpc.ankr.com/eth";
 
     @Operation(description = "account-balance", tags = "account-api")
     @RequestMapping(
@@ -18,10 +28,18 @@ public class AccountController {
             method = RequestMethod.GET
     )
     public ResponseEntity<String> getAccountBalance(
-            @Parameter(name = "apiKey", description = "Api Key") @RequestParam(value = "apiKey", required = true) final String apiKey,
-            @Parameter(name = "address", description = "Wallet Address") @RequestParam(value = "address", required = true) final String address) {
+            @Parameter(name = "address", description = "Wallet Address") @RequestParam(value = "address", required = true) final String address)
+            throws IOException {
 
-        return ResponseEntity.ok().body("");
+        Web3j web3 = Web3j.build(new HttpService(RPC_PROVIDER));
+
+        EthGetBalance balance = web3.ethGetBalance(address,
+                DefaultBlockParameterName.LATEST).send();
+
+        BigDecimal balanceInETH = Convert.fromWei(balance.getBalance().toString(),
+                Unit.ETHER);
+
+        return ResponseEntity.ok().body(balanceInETH.toString());
 
     }
 }
