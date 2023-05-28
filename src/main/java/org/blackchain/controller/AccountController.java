833e87ffb70c0cb1943372d7f00a3ebf053b5e77
samdann <sami.dannoune@gmail.com>
2023-05-28 15:59:16 +0100

    trying out etherscan API

--- src/main/java/org/blackchain/controller/AccountController.java
+++ src/main/java/org/blackchain/controller/AccountController.java
@@ -1,9 +1,11 @@
 package org.blackchain.controller;
 
+import io.goodforgod.api.etherscan.EtherScanAPI;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,15 +33,26 @@ public class AccountController {
             @Parameter(name = "address", description = "Wallet Address") @RequestParam(value = "address", required = true) final String address)
             throws IOException {
 
+        StringBuilder result = new StringBuilder();
+
         Web3j web3 = Web3j.build(new HttpService(RPC_PROVIDER));
 
         EthGetBalance balance = web3.ethGetBalance(address,
                 DefaultBlockParameterName.LATEST).send();
 
-        BigDecimal balanceInETH = Convert.fromWei(balance.getBalance().toString(),
-                Unit.ETHER);
+        BigDecimal web3BalanceInEth = Convert.fromWei(balance.getBalance().toString(),
+                Unit.WEI);
 
-        return ResponseEntity.ok().body(balanceInETH.toString());
+        EtherScanAPI api = EtherScanAPI.builder().build();
+        BigInteger etherScanBalanceInEth = api.account().balance(address).getBalanceInWei().asWei();
+
+        if (etherScanBalanceInEth.toString().equals(web3BalanceInEth.toString())) {
+            result.append(web3BalanceInEth);
+        } else {
+            result.append("NADA");
+        }
+
+        return ResponseEntity.ok().body(result.toString());
 
     }
 }
