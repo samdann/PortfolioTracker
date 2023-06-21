b8da4a32e10cc165d61ab7e7c7f2ccecef1e6505
samdann <sami.dannoune@gmail.com>
2023-06-21 18:32:34 +0200

    coinbase integration

--- src/main/java/org/blackchain/util/Requests.java
+++ src/main/java/org/blackchain/util/Requests.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 
 @Slf4j
@@ -15,12 +14,13 @@ public class Requests {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "");
+        //RequestBody body = RequestBody.create(mediaType, "");
         Request request = new Request.Builder()
                 .url("https://api.exchange.coinbase.com/accounts")
                 .addHeader("Content-Type", "application/json")
                 .build();
         Response response = client.newCall(request).execute();
+        assert response.body() != null;
         log.info(response.body().string());
 
     }
