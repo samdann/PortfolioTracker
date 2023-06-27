e46c9c1456350fa2e045700e5745c6e2fda31ca1
samdann <sami.dannoune@gmail.com>
2023-06-27 20:41:07 +0200

    cracked coinbase api signature issue

--- src/main/java/org/blackchain/util/Requests.java
+++ src/main/java/org/blackchain/util/Requests.java
@@ -2,7 +2,6 @@ package org.blackchain.util;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import java.util.Date;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -10,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.apache.commons.codec.digest.HmacUtils;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
@@ -21,10 +20,10 @@ import org.springframework.stereotype.Service;
 public class Requests {
 
     private static final String CB_ACCESS_KEY = "cb-access-key";
-    private static final String CB_ACCESS_PASSPHRASE = "cb-access-passphrase";
+    private static final String CB_ACCESS_SECRET = "cb-access-passphrase";
     private static final String CB_ACCESS_TIMESTAMP = "cb-access-timestamp";
     private static final String CB_ACCESS_SIGN = "cb-access-sign";
-    private static final String HMAC_SHA256_ALGO = "HmacSHA256";
+    private static final String HMAC_SHA_256 = "HmacSHA256";
 
 
     @Value("${coinbase.api.key}")
@@ -33,13 +32,13 @@ public class Requests {
     @Value("${coinbase.api.passphrase}")
     private String coinbaseApiPassPhrase;
 
-    private static byte[] calcHmacSha256(byte[] secretKey, byte[] message) {
-        byte[] hmacSha256 = null;
+    private static String calcHmacSha256(byte[] secretKey, String message) {
+        String hmacSha256 = null;
         try {
-            Mac mac = Mac.getInstance(HMAC_SHA256_ALGO);
-            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey, HMAC_SHA256_ALGO);
+            Mac mac = Mac.getInstance(HMAC_SHA_256);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey, HMAC_SHA_256);
             mac.init(secretKeySpec);
-            hmacSha256 = mac.doFinal(message);
+            hmacSha256 = Hex.encodeHexString(mac.doFinal(message.getBytes(StandardCharsets.UTF_8)));
 
 
         } catch (Exception e) {
@@ -53,16 +52,19 @@ public class Requests {
                 .build();
 
         String method = "GET";
+        String body = null;
+        String requestPath = "/v2/accounts";
         String timestamp = new Date().getTime() / 1000L + "";
-        String signature = getSignature(timestamp, "GET", "/address-book", "");
+        String signature = getSignature(timestamp, method, requestPath, body);
 
         Request request = new Request.Builder()
-                .url("https://api.exchange.coinbase.com/address-book")
-                .addHeader("Content-Type", "application/json")
+                .url("https://api.coinbase.com" + requestPath)
+                .addHeader("Content-Type", "application/json; charset=utf-8")
                 .addHeader(CB_ACCESS_KEY, coinbaseApiKey)
-                .addHeader(CB_ACCESS_PASSPHRASE, coinbaseApiPassPhrase)
+                .addHeader(CB_ACCESS_SECRET, coinbaseApiPassPhrase)
                 .addHeader(CB_ACCESS_TIMESTAMP, timestamp)
                 .addHeader(CB_ACCESS_SIGN, signature)
+                .addHeader("CB-VERSION", "2023-06-22")
                 .build();
         Response response = client.newCall(request).execute();
         assert response.body() != null;
@@ -72,15 +74,13 @@ public class Requests {
 
     private String getSignature(String timeStamp, String method, String path, String body) {
 
-        byte[] secretKey = Base64.getDecoder().decode(coinbaseApiKey.getBytes());
         String message = timeStamp + method + path + ((body == null) ? "" : body);
+        log.info(message);
+        return calcHmacSha256(coinbaseApiPassPhrase.getBytes(StandardCharsets.UTF_8), message);
+        //HmacUtils.
 
-        //byte[] hmacSha256 = calcHmacSha256(secretKey, message.getBytes(StandardCharsets.UTF_8));
-
-        return new HmacUtils(HMAC_SHA256_ALGO, secretKey).hmacHex(
-                message.getBytes(StandardCharsets.UTF_8));
         //return Hex.encodeHexString(hmacSha256);
-        //return Base64.getEncoder().encode
+        //return Base64.getEncoder().encodeToString(hmacSha256);
     }
 
 
