31fe28ecfd9d007a4bb76196ffe67480a83552eb
samdann <sami.dannoune@gmail.com>
2023-07-01 22:39:47 +0200

    little life hack: main class for silly test

--- src/main/java/org/blackchain/util/MainClass.java
+++ src/main/java/org/blackchain/util/MainClass.java
@@ -1,2 +1,12 @@
-package org.blackchain.util;public class MainClass {
+package org.blackchain.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        List<Long> longs = Arrays.asList(1l, 1000l, 100l, 10l);
+        longs.stream().sorted((x, y) -> Long.compare(y, x)).forEach(System.out::println);
+    }
 }
