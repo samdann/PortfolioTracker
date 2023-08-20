8abe7e461ee51a377c70c4e73b522a2a217a47a5
samdann <sami.dannoune@gmail.com>
2023-08-20 17:06:40 +0200

    enabling formatter markers

--- src/main/java/org/blackchain/model/transaction/EthTransaction.java
+++ src/main/java/org/blackchain/model/transaction/EthTransaction.java
@@ -24,34 +24,51 @@ public class EthTransaction extends Transaction {
      private BigInteger gasUsed;
 
      public static EthTransaction convertToEthTransaction(final Tx tx) {
-          return EthTransaction.builder().txHash(tx.getHash())
+          // @formatter:off
+          return EthTransaction.builder()
+                  .txHash(tx.getHash())
                   .blockNumber(BigInteger.valueOf(tx.getBlockNumber()))
                   .timestamp(Timestamp.valueOf(tx.getTimeStamp()).getTime())
-                  .from(tx.getFrom()).to(tx.getTo())
+                  .from(tx.getFrom())
+                  .to(tx.getTo())
                   .value(Convert.fromWei(tx.getValue().toString(), Unit.ETHER))
-                  .gas(tx.getGas().asWei()).gasUsed(tx.getGasUsed().asWei()).build();
-
+                  .gas(tx.getGas().asWei())
+                  .gasUsed(tx.getGasUsed().asWei())
+                  .build();
+          // @formatter:on
      }
 
      public static EthTransaction convertToEthTransaction(final TxInternal tx) {
-          return EthTransaction.builder().txHash(tx.getHash())
+          // @formatter:off
+          return EthTransaction.builder()
+                  .txHash(tx.getHash())
                   .blockNumber(BigInteger.valueOf(tx.getBlockNumber()))
                   .timestamp(Timestamp.valueOf(tx.getTimeStamp()).getTime())
-                  .from(tx.getFrom()).to(tx.getTo())
+                  .from(tx.getFrom())
+                  .to(tx.getTo())
                   .value(Convert.fromWei(tx.getValue().toString(), Unit.ETHER))
-                  .gas(tx.getGas().asWei()).gasUsed(tx.getGasUsed().asWei()).build();
-
+                  .gas(tx.getGas().asWei())
+                  .gasUsed(tx.getGasUsed().asWei())
+                  .build();
+          // @formatter:on
      }
 
      public static EthTransaction convertToEthTransaction(final TxErc20 tx) {
-          return EthTransaction.builder().txHash(tx.getHash())
+          // @formatter:off
+          return EthTransaction.builder()
+                  .txHash(tx.getHash())
                   .blockNumber(BigInteger.valueOf(tx.getBlockNumber()))
                   .timestamp(Timestamp.valueOf(tx.getTimeStamp()).getTime())
-                  .from(tx.getFrom()).to(tx.getTo())
+                  .from(tx.getFrom())
+                  .to(tx.getTo())
                   .value(EthereumUtils.convertWithTokenDecimal(tx.getValue().toString(),
-                          tx.getTokenDecimal())).gas(tx.getGas().asWei())
-                  .gasUsed(tx.getGasUsed().asWei()).tokenSymbol(tx.getTokenSymbol())
-                  .tokenName(tx.getTokenName()).build();
+                          tx.getTokenDecimal()))
+                  .gas(tx.getGas().asWei())
+                  .gasUsed(tx.getGasUsed().asWei())
+                  .tokenSymbol(tx.getTokenSymbol())
+                  .tokenName(tx.getTokenName())
+                  .build();
+          // @formatter:on
      }
 
 }
